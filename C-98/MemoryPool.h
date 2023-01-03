3aca1dc28994c6d8a7296e341a20fa6abfca9e62
Liuyuan <2787141886@qq.com>
2023-01-03 22:17:07 +0800

    Format codes

--- C-98/MemoryPool.h
+++ C-98/MemoryPool.h
@@ -26,65 +26,63 @@
 #include <limits.h>
 #include <stddef.h>
 
-template <typename T, size_t BlockSize = 4096>
-class MemoryPool
-{
-  public:
-    /* Member types */
-    typedef T               value_type;
-    typedef T*              pointer;
-    typedef T&              reference;
-    typedef const T*        const_pointer;
-    typedef const T&        const_reference;
-    typedef size_t          size_type;
-    typedef ptrdiff_t       difference_type;
+template <typename T, size_t BlockSize = 4096> class MemoryPool {
+public:
+  /* Member types */
+  typedef T value_type;
+  typedef T *pointer;
+  typedef T &reference;
+  typedef const T *const_pointer;
+  typedef const T &const_reference;
+  typedef size_t size_type;
+  typedef ptrdiff_t difference_type;
 
-    template <typename U> struct rebind {
-      typedef MemoryPool<U> other;
-    };
+  template <typename U> struct rebind {
+    typedef MemoryPool<U> other;
+  };
 
-    /* Member functions */
-    MemoryPool() throw();
-    MemoryPool(const MemoryPool& memoryPool) throw();
-    template <class U> MemoryPool(const MemoryPool<U>& memoryPool) throw();
+  /* Member functions */
+  MemoryPool() throw();
+  MemoryPool(const MemoryPool &memoryPool) throw();
+  template <class U> MemoryPool(const MemoryPool<U> &memoryPool) throw();
 
-    ~MemoryPool() throw();
+  ~MemoryPool() throw();
 
-    pointer address(reference x) const throw();
-    const_pointer address(const_reference x) const throw();
+  pointer address(reference x) const throw();
+  const_pointer address(const_reference x) const throw();
 
-    // Can only allocate one object at a time. n and hint are ignored
-    pointer allocate(size_type n = 1, const_pointer hint = 0);
-    void deallocate(pointer p, size_type n = 1);
+  // Can only allocate one object at a time. n and hint are ignored
+  pointer allocate(size_type n = 1, const_pointer hint = 0);
+  void deallocate(pointer p, size_type n = 1);
 
-    size_type max_size() const throw();
+  size_type max_size() const throw();
 
-    void construct(pointer p, const_reference val);
-    void destroy(pointer p);
+  void construct(pointer p, const_reference val);
+  void destroy(pointer p);
 
-    pointer newElement(const_reference val);
-    void deleteElement(pointer p);
+  pointer newElement(const_reference val);
+  void deleteElement(pointer p);
 
-  private:
-    union Slot_ {
-      value_type element;
-      Slot_* next;
-    };
+private:
+  union Slot_ {
+    value_type element;
+    Slot_ *next;
+  };
 
-    typedef char* data_pointer_;
-    typedef Slot_ slot_type_;
-    typedef Slot_* slot_pointer_;
+  typedef char *data_pointer_;
+  typedef Slot_ slot_type_;
+  typedef Slot_ *slot_pointer_;
 
-    slot_pointer_ currentBlock_;
-    slot_pointer_ currentSlot_;
-    slot_pointer_ lastSlot_;
-    slot_pointer_ freeSlots_;
+  slot_pointer_ currentBlock_;
+  slot_pointer_ currentSlot_;
+  slot_pointer_ lastSlot_;
+  slot_pointer_ freeSlots_;
 
-    size_type padPointer(data_pointer_ p, size_type align) const throw();
-    void allocateBlock();
-   /*
-    static_assert(BlockSize >= 2 * sizeof(slot_type_), "BlockSize too small.");
-    */
+  size_type padPointer(data_pointer_ p, size_type align) const throw();
+  void allocateBlock();
+  /*
+   static_assert(BlockSize >= 2 * sizeof(slot_type_), "BlockSize too small.");
+   */
 };
 
 #include "MemoryPool.tcc"
